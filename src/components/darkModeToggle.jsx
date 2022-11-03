92b8de22a29707cbd0b51daa42640930746a0f69
J S Vignesh Kanna <vkrishitaran@gmail.com>
2022-11-03 23:23:28 +0530

    darkmode toggler added

---
+++ src/components/darkModeToggle.jsx
@@ -0,0 +1,20 @@
+import { useTheme } from "../hooks/useTheme";
+import { HiSun } from "react-icons/hi/index.js";
+import { BsFillMoonStarsFill } from "react-icons/bs/index.js";
+
+const DarkModeToggle = () => {
+  const { theme, toggle } = useTheme();
+  const Icon = theme === "light" ? BsFillMoonStarsFill : HiSun;
+  return (
+    <button
+      aria-label="toggle-darkmode"
+      type="button"
+      onClick={toggle}
+      className="p-2 hover:scale-110 active:scale-90 transition-transform text-sky-400 dark:text-orange-600"
+    >
+      <Icon size={26} />
+    </button>
+  );
+};
+
+export default DarkModeToggle;
