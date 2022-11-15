76cba5121ae2b048b4bfb987f2b7abfcc60f0654
J S Vignesh Kanna <vkrishitaran@gmail.com>
2022-11-15 23:28:25 +0530

    index import

--- src/components/HamburgerMenu.jsx
+++ src/components/HamburgerMenu.jsx
@@ -1,4 +1,4 @@
-import { MdClose } from "react-icons/md";
+import { MdClose } from "react-icons/md/index";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { useState, useEffect } from "react";
 import NavLink from "./NavLink.astro";
