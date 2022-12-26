298baea480c28029f7a7851a11e57aafb991e347
J S Vignesh Kanna <vkrishitaran@gmail.com>
2022-12-26 21:02:41 +0530

    react, tailwind, mdx added

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,4 +1,20 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import react from "@astrojs/react";
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+import netlify from "@astrojs/netlify/functions";
+
+// https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react(), tailwind(), mdx()],
+  output: "server",
+  adapter: netlify()
+});
