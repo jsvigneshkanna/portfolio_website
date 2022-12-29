93b39d6fe7aa0d11a23b3a3e8752bf0a79dfd9c5
J S Vignesh Kanna <vkrishitaran@gmail.com>
2022-12-29 19:20:48 +0530

    mobile navbar added

--- client/src/components/MobileNavbar.tsx
+++ client/src/components/MobileNavbar.tsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import MobileNavLinks from "./MobileNavLinks";
+import DarkModeToggle from "./darkModeToggle";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { RiCloseFill } from "react-icons/ri";
 
 type Props = {};
 
@@ -23,11 +26,30 @@ const MobileNavbar = (props: Props) => {
     },
   ];
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  return (
-    <div>
-      {navLinks.map((navLink) => (
-        <MobileNavLinks navHeader={navLink["name"]} navHref={navLink["href"]} />
+  const MenuIcon = isMenuOpen ? <RiCloseFill size={24} /> : <GiHamburgerMenu size={24} />;
+  return isMenuOpen ? (
+    <div
+      className="flex flex-col justify-center items-center gap-20 py-20 mx-auto absolute top-0 left-0 w-full
+    dark:bg-gradient-to-br dark:from-purple-900 bg-gradient-to-bl from-blue-300 to-orange-400 dark:to-cyan-800 z-10"
+    >
+      <div className="absolute top-10 left-6">
+        <DarkModeToggle />
+      </div>
+      <div className="absolute top-12 right-6" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+        {MenuIcon}
+      </div>
+      {navLinks.map((navLink, index) => (
+        <MobileNavLinks navHeader={navLink["name"]} navHref={navLink["href"]} key={index} />
       ))}
+    </div>
+  ) : (
+    <div>
+      <div className="absolute top-10 right-6">
+        <div onClick={() => setIsMenuOpen(!isMenuOpen)}>{MenuIcon}</div>
+      </div>
+      <div className="absolute top-8 left-6">
+        <DarkModeToggle />
+      </div>
     </div>
   );
 };
