2fa85b8eb1cf40b12c2f57cf438db01480b363c6
J S Vignesh Kanna <vkrishitaran@gmail.com>
2022-12-29 19:37:22 +0530

    framer motion added for mobile nav open

--- client/src/components/Navbars/MobileNavbar.tsx
+++ client/src/components/Navbars/MobileNavbar.tsx
@@ -3,6 +3,7 @@ import MobileNavLinks from "./MobileNavLinks";
 import DarkModeToggle from "./darkModeToggle";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { RiCloseFill } from "react-icons/ri";
+import { motion as fm } from "framer-motion";
 
 type Props = {};
 
@@ -27,8 +28,23 @@ const MobileNavbar = (props: Props) => {
   ];
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const MenuIcon = isMenuOpen ? <RiCloseFill size={24} /> : <GiHamburgerMenu size={24} />;
+  const openAnimationConfiguration = {
+    initial: { opacity: 0 },
+    animate: { opacity: 1 },
+    exit: { opacity: 0 },
+  };
+  const closeAnimationConfiguration = {
+    initial: { opacity: 0 },
+    animate: { opacity: 1 },
+    exit: { opacity: 0 },
+  };
   return isMenuOpen ? (
-    <div
+    <fm.div
+      variants={openAnimationConfiguration}
+      initial="initial"
+      animate="animate"
+      exit="exit"
+      transition={{ duration: 0.6 }}
       className="flex flex-col justify-center items-center gap-20 py-20 pt-40 mx-auto absolute top-0 left-0 w-full
     dark:bg-gradient-to-br dark:from-purple-900 bg-gradient-to-bl from-blue-300 to-orange-400 dark:to-cyan-800 z-10"
     >
@@ -41,7 +57,7 @@ const MobileNavbar = (props: Props) => {
       {navLinks.map((navLink, index) => (
         <MobileNavLinks navHeader={navLink["name"]} navHref={navLink["href"]} key={index} />
       ))}
-    </div>
+    </fm.div>
   ) : (
     <div>
       <div className="absolute top-10 right-6">
