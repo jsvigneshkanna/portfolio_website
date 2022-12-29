8cf9cfb46bf8966715d4b9f024af6ebd0e473635
J S Vignesh Kanna <vkrishitaran@gmail.com>
2022-12-29 15:34:07 +0530

    home banner added

---
+++ client/src/components/HomeIntro.tsx
@@ -0,0 +1,44 @@
+import vk_image from "../assets/vk_banner_image.jpg";
+import Typewriter from "typewriter-effect";
+
+type Props = {};
+
+const HomeIntro = (props: Props) => {
+  return (
+    <div className="flex flex-col-reverse lg:flex-row justify-between items-center h-[80vh] px-4 md:px-10 lg:px-60 2xl:px-72 gap-20 md:gap-8 flex-shrink-0">
+      <div className=" min-w-max">
+        <img
+          src={vk_image}
+          alt="vignesh kanna image"
+          className=" rounded-[50%] w-60 h-60 lg:w-80 lg:h-80 object-fill"
+        />
+      </div>
+      <div className="flex flex-col gap-8">
+        <h1 className=" text-3xl lg:text-4xl md:text-4xl 2xl:text-7xl font-black tracking-widest">J S Vignesh Kanna</h1>
+        <h3 className="text-lg lg:text-xl md:text-xl 2xl:text-3xl font-semibold tracking-widest text-red-400 dark:text-blue-500">
+          <Typewriter
+            options={{
+              strings: [
+                "I am a Software Engineer 🧑‍💻",
+                "I am a Freelancer 🎨",
+                "I am a Content Creator 📸",
+                "I am a Blogger ✍️",
+              ],
+              cursor: " | ",
+              delay: 40,
+              deleteSpeed: 30,
+              loop: true,
+              autoStart: true,
+            }}
+          />
+        </h3>
+        <p className="font-medium text-lg ">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non obcaecati suscipit, officiis dolor, eaque labore
+          rerum cum consectetur maxime quae vero sit eveniet qui aut. Eaque neque soluta facilis adipisci.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default HomeIntro;
--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -3,6 +3,13 @@ module.exports = {
   darkMode: "class",
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1600px",
+    },
     extend: {},
   },
   plugins: [],
