08c16c2be5d9540b9111b9e5651dfa3f6ba84455
J S Vignesh Kanna <vkrishitaran@gmail.com>
2022-12-30 13:04:55 +0530

    theme toggle icons changed to fa

--- client/src/components/Navbars/darkModeToggle.tsx
+++ client/src/components/Navbars/darkModeToggle.tsx
@@ -1,17 +1,16 @@
 import { useTheme } from "../../hooks/useTheme";
-import { HiSun } from "react-icons/hi/index.js";
-import { BsFillMoonStarsFill } from "react-icons/bs/index.js";
+import { FaMoon, FaSun } from "react-icons/fa/index.js";
 
 const DarkModeToggle = () => {
   const { theme, toggle } = useTheme();
 
-  const Icon = theme === "light" ? BsFillMoonStarsFill : HiSun;
+  const Icon = theme === "light" ? FaMoon : FaSun;
   return (
     <button
       aria-label="toggle-darkmode"
       type="button"
       onClick={toggle}
-      className="p-2 hover:scale-110 active:scale-90 transition-transform text-slate-800 dark:text-yellow-200"
+      className="p-2 hover:scale-110 active:scale-90 transition-transform text-slate-800 dark:text-slate-100"
     >
       <Icon size={26} />
     </button>
