393a43bc663b8f279dabeaad3cd8644577427d10
J S Vignesh Kanna <vkrishitaran@gmail.com>
2022-12-31 23:46:47 +0530

    py3 added project card in mobile

--- client/src/components/projectspage/ProjectCard.tsx
+++ client/src/components/projectspage/ProjectCard.tsx
@@ -15,7 +15,7 @@ const ProjectCard = (props: Props) => {
   return (
     <div className=" w-[280px] md:w-[400px] bg-zinc-100 hover:bg-zinc-200 dark:bg-slate-800 dark:hover:bg-slate-700 rounded-lg overflow-hidden flex-grow hover:scale-105">
       <img src={urlFor(projectDetails.thumbnailImage.asset._ref).url()} alt="project image" />
-      <div className="w-[90%] flex flex-col justify-center items-center mx-auto py-2 md:py-5 gap-4">
+      <div className="w-[90%] flex flex-col justify-center items-center mx-auto py-3 md:py-5 gap-4">
         <h3 className=" font-extrabold text-lg md:text-xl text-center text-blue-600 dark:text-orange-600">
           {projectDetails.title}
         </h3>
