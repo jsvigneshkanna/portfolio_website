a378ba4c235a3e56066a3e4bbcb9c5d25e061a8a
J S Vignesh Kanna <vkrishitaran@gmail.com>
2023-01-01 12:23:30 +0530

    demo and github link bg color updated

--- client/src/components/buttons/GithubBtn.tsx
+++ client/src/components/buttons/GithubBtn.tsx
@@ -2,7 +2,7 @@ type Props = {};
 
 const GithubBtn = (props: Props) => {
   return (
-    <div className=" bg-blue-900 dark:bg-purple-600 text-white font-medium text-lg rounded-lg shadow-md shadow-blue-800/70 px-2 py-1">
+    <div className=" bg-blue-700 dark:bg-purple-700 text-white font-medium text-lg rounded-lg shadow-md shadow-blue-800/70 px-2 py-1">
       Github
     </div>
   );
