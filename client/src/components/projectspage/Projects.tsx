79db0f9ae8ad62c97e51f8d46aab1963f8f9672d
J S Vignesh Kanna <vkrishitaran@gmail.com>
2023-01-01 12:08:40 +0530

    no project text added

--- client/src/components/projectspage/Projects.tsx
+++ client/src/components/projectspage/Projects.tsx
@@ -26,6 +26,8 @@ const Projects = (props: Props) => {
   return (
     <div className="px-8 md:px-10 lg:px-60 2xl:px-72 mt-8">
       {/* <TechFilter setFilterValue={setFilter} /> */}
+
+      {/* Tech filters */}
       <div className="flex flex-row flex-wrap bg-slate-200 dark:bg-slate-700 px-2 py-2 rounded-3xl gap-4 justify-start items-center">
         <button
           className=" min-w-max px-2 bg-slate-700 dark:bg-slate-200 text-white dark:text-black rounded-3xl border-[1px] border-blue-500"
@@ -82,10 +84,21 @@ const Projects = (props: Props) => {
           AstroDEV
         </button>
       </div>
+
+      <p className="mt-4 font-bold text-lg">
+        <span className="underline underline-offset-8 decoration-red-500 uppercase decoration-[2px]">{filter}</span>{" "}
+        projects shown 🔥
+      </p>
       <div className="flex flex-wrap gap-10 justify-between items-center mt-12">
-        {projects.map((project) => {
-          return <ProjectCard projectDetails={project} key={project._id} />;
-        })}
+        {projects.length != 0 ? (
+          projects.map((project) => {
+            return <ProjectCard projectDetails={project} key={project._id} />;
+          })
+        ) : (
+          <p className="flex justify-center items-center mx-auto bg-blue-600 text-black p-5 rounded-3xl shadow-lg shadow-blue-400 dark:shadow-blue-800 mt-6 font-semibold text-lg text-center">
+            😥 No projects done in this technology, Will sure make one in this for you 🫵
+          </p>
+        )}
       </div>
     </div>
   );
