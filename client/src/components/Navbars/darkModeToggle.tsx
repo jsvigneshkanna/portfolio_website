306f49cffa8ce27652036876b4ffa1e771148429
J S Vignesh Kanna <vkrishitaran@gmail.com>
2023-02-25 21:05:36 +0530

    dark mode toggler updated

--- client/src/components/Navbars/darkModeToggle.tsx
+++ client/src/components/Navbars/darkModeToggle.tsx
@@ -1,16 +1,16 @@
 import { useTheme } from "../../hooks/useTheme";
-import { FaMoon, FaSun } from "react-icons/fa/index.js";
+import { RiSunFill, RiMoonFill } from "react-icons/ri/index.js";
 
 const DarkModeToggle = () => {
   const { theme, toggle } = useTheme();
 
-  const Icon = theme === "light" ? FaMoon : FaSun;
+  const Icon = theme === "light" ? RiMoonFill : RiSunFill;
   return (
     <button
       aria-label="toggle-darkmode"
       type="button"
       onClick={toggle}
-      className="p-2 hover:scale-110 active:scale-90 transition-transform text-slate-800 dark:text-slate-100"
+      className="p-2 hover:scale-110 active:scale-90 transition-transform text-slate-100 hover:text-slate-200 dark:text-yellow-400 dark:hover:text-yellow-300"
     >
       <Icon size={26} />
     </button>
