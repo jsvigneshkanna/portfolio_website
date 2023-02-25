cf4a0b503867c8fe797284867bf0ca2b1be41c11
J S Vignesh Kanna <vkrishitaran@gmail.com>
2023-02-25 21:48:59 +0530

    mobile navbar gap added and delay slowed

--- client/src/components/Navbars/MobileNavbar.tsx
+++ client/src/components/Navbars/MobileNavbar.tsx
@@ -44,8 +44,8 @@ const MobileNavbar = (props: Props) => {
       initial="initial"
       animate="animate"
       exit="exit"
-      transition={{ duration: 0.8 }}
-      className="flex flex-col justify-start items-center py-44 gap-16 mx-auto absolute top-0 left-0 w-full h-[100vh] dark:bg-gradient-to-br dark:from-fuchsia-900 bg-gradient-to-bl from-rose-300 to-blue-200 dark:to-blue-800 z-10"
+      transition={{ duration: 0.6 }}
+      className="flex flex-col justify-start items-center pt-44 gap-20 mx-auto absolute top-0 left-0 w-full h-[100vh] dark:bg-gradient-to-br dark:from-fuchsia-900 bg-gradient-to-bl from-rose-300 to-blue-200 dark:to-blue-800 z-10"
     >
       <div className="absolute top-12 left-6">
         <DarkModeToggle />
