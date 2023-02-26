eaab6074c81551957d2fca800e0d03fb7fb4d9b5
J S Vignesh Kanna <vkrishitaran@gmail.com>
2023-02-26 14:58:15 +0530

    work experience skills added

--- client/src/components/homepage/WorkExperienceSection.tsx
+++ client/src/components/homepage/WorkExperienceSection.tsx
@@ -14,7 +14,7 @@ const WorkExperienceSection = (props: Props) => {
   return (
     <div className="w-[90%] md:w-[46%]">
       <div className="flex flex-row justify-start items-center gap-3 w-full ">
-        <p className="w-6 md:w-8 h-6 md:h-8 bg-sky-300 shadow-md shadow-slate-900/30 dark:bg-orange-500 dark:shadow-fuchsia-900/60 rounded-full flex justify-center items-center font-medium">
+        <p className="w-6 md:w-8 h-6 md:h-8 bg-indigo-200 shadow-md shadow-slate-900/30 dark:bg-orange-500 dark:shadow-fuchsia-900/60 rounded-full flex justify-center items-center font-medium">
           {props.id}
         </p>
         <p className=" w-[calc(100%-28px)] md:w-[calc(100%-36px)] text-slate-900 dark:text-slate-50 font-semibold tracking-normal lg:tracking-wide text-base lg:text-lg ">
@@ -27,6 +27,16 @@ const WorkExperienceSection = (props: Props) => {
       <p className="text-sm md:text-[15px] 2xl:text-base font-normal text-slate-800 dark:text-slate-300 mt-2">
         {props.description}
       </p>
+      <div className="flex flex-row flex-wrap justify-start items-center gap-2 mt-3">
+        <p className="text-sm md:text-base">Skills: </p>
+        {props.skills.map((skill) => {
+          return (
+            <p className="px-2 py-1 rounded-md bg-indigo-200 shadow-md shadow-slate-900/30 dark:bg-slate-800 dark:shadow-slate-900 text-xs md:text-sm font-medium capitalize">
+              {skill}
+            </p>
+          );
+        })}
+      </div>
     </div>
   );
 };
