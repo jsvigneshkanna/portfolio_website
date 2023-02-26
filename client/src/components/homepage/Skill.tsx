1bd9bcfaa2df8465158526a143c93e114922bac5
J S Vignesh Kanna <vkrishitaran@gmail.com>
2023-02-26 20:15:28 +0530

    skillset ipad screen adjusted

--- client/src/components/homepage/Skill.tsx
+++ client/src/components/homepage/Skill.tsx
@@ -22,6 +22,7 @@ import {
   SiCraftcms,
 } from "react-icons/si/index.js";
 import { RiReactjsLine } from "react-icons/ri/index.js";
+
 type Props = {
   name: string;
   value: string;
@@ -52,13 +53,17 @@ const skill = {
 const Skill = (props: Props) => {
   return (
     <div className=" group relative cursor-pointer flex">
-      <div className="rounded-full border border-slate-320 bg-slate-800 dark:bg-indigo-100 object-cover filter group-hover:grayscale transition duration-500 ease-in-out p-6 md:p-10 text-slate-200 dark:text-slate-900">
+      <div className="rounded-full border border-slate-320 bg-slate-800 dark:bg-indigo-100 object-cover filter group-hover:grayscale transition duration-500 ease-in-out p-6 md:p-7 lg:p-10 text-slate-200 dark:text-slate-900">
         {skill[props.name.toLowerCase()]}
       </div>
       <div className="absolute opacity-0 group-hover:opacity-90 transition duration-320 ease-in-out dark:group-hover:bg-indigo-100 group-hover:bg-blue-900 w-full h-full rounded-full">
         <div className="flex flex-col items-center justify-center h-full">
-          <p className="text-sm md:text-lg font-medium text-slate-200 dark:text-red-600 opacity-100">{props.name}</p>
-          <p className="text-base md:text-2xl font-bold text-yellow-400 dark:text-black opacity-100">{props.value}</p>
+          <p className="text-sm md:text-base lg:text-lg font-medium text-slate-200 dark:text-red-600 opacity-100">
+            {props.name}
+          </p>
+          <p className="text-base md:text-lg lg:text-2xl font-bold text-yellow-400 dark:text-black opacity-100">
+            {props.value}
+          </p>
         </div>
       </div>
     </div>
