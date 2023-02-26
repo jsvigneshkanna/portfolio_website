2f9b04ee506f6dd1acc11d69b0fdb5b39896b028
J S Vignesh Kanna <vkrishitaran@gmail.com>
2023-02-26 14:49:22 +0530

    work experience updated for small devices

--- client/src/components/homepage/WorkExperienceSection.tsx
+++ client/src/components/homepage/WorkExperienceSection.tsx
@@ -12,16 +12,18 @@ type Props = {
 
 const WorkExperienceSection = (props: Props) => {
   return (
-    <div className="w-[46%]">
-      <div className="flex flex-row justify-start items-center gap-3 w-full">
-        <p className="w-6 md:w-8 h-6 md:h-8 bg-orange-500 rounded-full flex justify-center items-center font-medium">
+    <div className="w-[90%] md:w-[46%]">
+      <div className="flex flex-row justify-start items-center gap-3 w-full ">
+        <p className="w-6 md:w-8 h-6 md:h-8 bg-sky-300 shadow-md shadow-slate-900/30 dark:bg-orange-500 dark:shadow-fuchsia-900/60 rounded-full flex justify-center items-center font-medium">
           {props.id}
         </p>
-        <p className="text-slate-900 dark:text-slate-50 font-semibold tracking-wide text-base lg:text-lg ">
+        <p className=" w-[calc(100%-28px)] md:w-[calc(100%-36px)] text-slate-900 dark:text-slate-50 font-semibold tracking-normal lg:tracking-wide text-base lg:text-lg ">
           {props.companyName} · {props.designation}
         </p>
       </div>
-      <p className="text-fuchsia-800 dark:text-cyan-500  tracking-wider font-medium mt-2">{props.duration}</p>
+      <p className="text-fuchsia-800 dark:text-cyan-500  tracking-wider font-normal md:font-medium mt-2">
+        {props.duration}
+      </p>
       <p className="text-sm md:text-[15px] 2xl:text-base font-normal text-slate-800 dark:text-slate-300 mt-2">
         {props.description}
       </p>
--- client/src/components/homepage/WorkExperiences.tsx
+++ client/src/components/homepage/WorkExperiences.tsx
@@ -6,17 +6,17 @@ type Props = {};
 const WorkExperiences = (props: Props) => {
   return (
     <div className="mt-24 px-6 md:px-10 lg:px-60 2xl:px-72">
-      <p className="text-lg md:text-xl lg:text-2xl font-bold underline underline-offset-8 decoration-cyan-500 dark:decoration-blue-500">
+      <p className="text-lg md:text-xl lg:text-2xl font-bold underline underline-offset-8 decoration-cyan-500 dark:decoration-blue-500 text-center lg:text-left">
         My Journey into software engineering
       </p>
-      <div className="w-full mt-6 flex flex-col justify-center items-start relative gap-8 py-14">
+      <div className="w-full mt-6 flex flex-col justify-center items-start relative gap-8 py-5 md:py-16">
         {/* middle line */}
-        <div className="w-[6px] bg-gradient-to-b from-cyan-500 to-orange-500 dark:from-blue-500 dark:to-rose-500 h-full rounded-full absolute top-0 left-[50%]"></div>
+        <div className="w-1 md:w-[6px] bg-gradient-to-b from-cyan-500 to-orange-500 dark:from-blue-500 dark:to-rose-500 h-full rounded-full absolute top-0 left-4 md:left-[50%]"></div>
 
         {workExperiences.map((workExperience) => {
           if (workExperience.id % 2 != 0) {
             return (
-              <div className="w-full flex flex-row justify-start items-center">
+              <div className="w-full flex flex-row ml-10 md:ml-0 justify-start md:justify-start items-center">
                 <WorkExperienceSection
                   id={workExperience.id}
                   companyName={workExperience.companyName}
@@ -30,7 +30,7 @@ const WorkExperiences = (props: Props) => {
             );
           } else {
             return (
-              <div className="w-full flex flex-row justify-end items-center">
+              <div className="w-full flex flex-row ml-10 md:ml-0 justify-start md:justify-end items-center">
                 <WorkExperienceSection
                   id={workExperience.id}
                   companyName={workExperience.companyName}
@@ -44,96 +44,6 @@ const WorkExperiences = (props: Props) => {
             );
           }
         })}
-        {/*         
-        <div className="w-full flex flex-row justify-start items-center">
-          <div className="w-[46%]">
-            <div className="flex flex-row justify-start items-center gap-3 w-full">
-              <p className="w-8 h-8 bg-orange-500 rounded-full flex justify-center items-center font-medium">1</p>
-              <p className="text-slate-900 dark:text-slate-50 font-semibold tracking-wide">
-                ALFA TKG integrated solutions India · Internship
-              </p>
-            </div>
-            <p className="text-slate-700 dark:text-blue-200 text-normal tracking-wider font-medium mt-2">
-              Jun 2019 - Jul 2019 (2 months)
-            </p>
-            <p className="text-sm font-normal text-slate-800 dark:text-slate-300 mt-2">
-              I worked in Image processing core team, where we developed a internal tool to detect the engineering
-              drawings from civil/ mechanical sheets using C# language and OCR technology
-            </p>
-          </div>
-        </div>
-
-        <div className="w-full flex flex-row justify-end items-center">
-          <div className="w-[46%]">
-            <div className="flex flex-row justify-start items-center gap-3 w-full">
-              <p className="w-8 h-8 bg-blue-500 rounded-full flex justify-center items-center font-medium">2</p>
-              <p className="text-slate-900 dark:text-slate-50 font-semibold tracking-wide">
-                Indian Institute of Science (IISc) · Winter Intern
-              </p>
-            </div>
-            <p className="text-slate-700 dark:text-blue-200 text-normal tracking-wider font-medium mt-2">
-              December 2019 - February 2010 (2 months)
-            </p>
-            <p className="text-sm font-normal text-slate-800 dark:text-slate-300 mt-2">
-              Researched and developed a new Aspect independent face recognition model using machine learning framework
-              under Honorary N. Balakrishnan professor in SERC lab.
-            </p>
-          </div>
-        </div>
-
-        <div className="w-full flex flex-row justify-start items-center">
-          <div className="w-[46%]">
-            <div className="flex flex-row justify-start items-center gap-3 w-full">
-              <p className="w-8 h-8 bg-teal-500 rounded-full flex justify-center items-center font-medium">1</p>
-              <p className="text-slate-900 dark:text-slate-50 font-semibold tracking-wide">
-                Gain Credit · Data Engineering Intern
-              </p>
-            </div>
-            <p className="text-slate-700 dark:text-blue-200 text-normal tracking-wider font-medium mt-2">
-              January 2021 - May 2021 (5 months)
-            </p>
-            <p className="text-sm font-normal text-slate-800 dark:text-slate-300 mt-2">
-              Worked in Core data engineering team comprising of 12 members as an entry level intern. Finished the
-              Apache Airflow project single handedly in span of 3 months comprising
-            </p>
-          </div>
-        </div>
-
-        <div className="w-full flex flex-row justify-end items-center">
-          <div className="w-[46%]">
-            <div className="flex flex-row justify-start items-center gap-3 w-full">
-              <p className="w-8 h-8 bg-rose-500 rounded-full flex justify-center items-center font-medium">4</p>
-              <p className="text-slate-900 dark:text-slate-50 font-semibold tracking-wide">
-                Gain Credit · Data Engineering Intern
-              </p>
-            </div>
-            <p className="text-slate-700 dark:text-blue-200 text-normal tracking-wider font-medium mt-2">
-              January 2021 - May 2021 (5 months)
-            </p>
-            <p className="text-sm font-normal text-slate-800 dark:text-slate-300 mt-2">
-              Worked in Core data engineering team comprising of 12 members as an entry level intern. Finished the
-              Apache Airflow project single handedly in span of 3 months comprising
-            </p>
-          </div>
-        </div>
-
-        <div className="w-full flex flex-row justify-start items-center">
-          <div className="w-[46%]">
-            <div className="flex flex-row justify-start items-center gap-3 w-full">
-              <p className="w-8 h-8 bg-slate-700 rounded-full flex justify-center items-center font-medium">5</p>
-              <p className="text-slate-900 dark:text-slate-50 font-semibold tracking-wide">
-                Gain Credit · Data Engineering Intern
-              </p>
-            </div>
-            <p className="text-slate-700 dark:text-blue-200 text-normal tracking-wider font-medium mt-2">
-              January 2021 - May 2021 (5 months)
-            </p>
-            <p className="text-sm font-normal text-slate-800 dark:text-slate-300 mt-2">
-              Worked in Core data engineering team comprising of 12 members as an entry level intern. Finished the
-              Apache Airflow project single handedly in span of 3 months comprising
-            </p>
-          </div>
-        </div>*/}
       </div>
     </div>
   );
