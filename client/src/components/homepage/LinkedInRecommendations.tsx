6925543e8feb2c97ebd665cd6275c0066ca66b28
J S Vignesh Kanna <vkrishitaran@gmail.com>
2023-02-26 17:36:26 +0530

    recommendations framer motion added

--- client/src/components/homepage/LinkedInRecommendations.tsx
+++ client/src/components/homepage/LinkedInRecommendations.tsx
@@ -1,4 +1,6 @@
 import LinkedInRecommnedation from "../../assets/linkedin_recommendations.png";
+import { motion } from "framer-motion";
+
 type Props = {};
 
 const LinkedInRecommendations = (props: Props) => {
@@ -7,18 +9,29 @@ const LinkedInRecommendations = (props: Props) => {
       <p className="text-lg md:text-xl lg:text-2xl font-bold underline underline-offset-8 decoration-fuchsia-500 dark:decoration-yellow-500 text-center lg:text-left">
         My LinkedIn Recommendations
       </p>
-      <a
-        href="https://www.linkedin.com/in/jsvigneshkanna/"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="flex justify-center items-center mt-10"
+      <motion.div
+        className="mt-10 lg:mt-14 w-full"
+        initial={{
+          x: -200,
+          y: 50,
+          opacity: 0,
+        }}
+        whileInView={{ opacity: 1, x: 0, y: 0 }}
+        transition={{ duration: 1 }}
       >
-        <img
-          src={LinkedInRecommnedation}
-          alt="linkedin recommendation"
-          className="w-[90%] md:w-[75%] rounded-lg object-cover shadow-md shadow-slate-900/40 hover:shadow-lg hover:shadow-slate-900/60 dark:shadow-blue-600/50"
-        />
-      </a>
+        <a
+          href="https://www.linkedin.com/in/jsvigneshkanna/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex justify-center items-center"
+        >
+          <img
+            src={LinkedInRecommnedation}
+            alt="linkedin recommendation"
+            className="w-[100%] md:w-[75%] rounded-lg object-cover shadow-md shadow-slate-900/40 hover:shadow-lg hover:shadow-slate-900/60 dark:shadow-blue-600/50"
+          />
+        </a>
+      </motion.div>
     </div>
   );
 };
